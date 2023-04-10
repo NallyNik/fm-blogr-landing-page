d4b59849a3cf08286065fdd82257e06eb66bc7d9
NallyNik <nallynik@gmail.com>
2023-04-10 18:08:48 -0300

    submenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,28 @@
 const btn = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
+const item = document.getElementsByClassName('menu-item'); 
 
 btn.addEventListener('click', () => {
-    const isOpen = menu.classList.contains('menu-open');
+    const isOpen = menu.classList.contains('open');
 
-    if (isOpen) menu.classList.remove('menu-open');
-    else menu.classList.add('menu-open');
+    if (isOpen) {
+        menu.classList.remove('open');
+        btn.style.background = 'url("assets/images/icon-hamburger.svg") no-repeat';
+    } else {
+        menu.classList.add('open');
+        btn.style.background = 'url("assets/images/icon-close.svg") no-repeat';
+    }
 })
+
+for (let i = 0; i < item.length; i++) {
+    item[i].addEventListener('click', () => {
+        const content = item[i].nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+            item[i].classList.toggle('open');
+        } else {
+            content.style.display = 'block';
+            item[i].classList.toggle('open');
+        }
+    })
+}
